3c77477a20e2a76d8d9769e50c1d5320ee53f1f2
cordellec <cordellec.charbonneau@hotmail.com>
2024-01-12 19:14:17 -0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+  <slot />
+
+  <div class="p-4 rounded-sm mt-16">
+    <div class="mt-8 flex justify-center items-center space-x-4">
+      <div></div>
+      <div class="btn-group variant-ghost-surface text-center text-black">
+        <button class="w-full">About</button>
+        <button class="w-full">Mint</button>
+        <button class="w-full"><LightSwitch></LightSwitch></button>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="mt-12 flex justify-center items-center space-x-4">
+  </div>
+
+  <svelte:fragment slot="pageFooter">
+	<div class="mt-12 flex justify-center items-center space-x-4">
+	  <img src="../icons/discord.png" alt="xrp" class="w-15 h-12" />
+	  <img src="../icons/opensea.png" alt="xrp" class="w-15 h-10" />
+	  <img src="../icons/x.png" alt="xrp" class="w-15 h-10" />
+	  <img src="../icons/etsy.png" alt="xrp" class="w-15 h-10" />
+	</div>
+  </svelte:fragment>
+  
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<!-- Full-screen layout -->
+<div class="hidden md:block">
+	<div class="grid grid-cols-5 mx-auto mt-24 ">
+	  <div class="col-start-1 col-span-2 ml-8 flex justify-center items-center pr-32">
+		<h1 class="text-center text-black" id="hero">GIVING CRYPTO <br /> A FACE THROUGH <br /> THROUGH TOKENEYES</h1>
+	  </div>
+  
+	  <div class="col-start-3 flex justify-center mt-12">
+		<img src="../images/eth.png" alt="xrp" class="max-w-full md:max-w-sm lg:max-w-full" />
+	  </div>
+  
+	  <div class="col-start-4 col-span-2 ml-8 flex justify-center items-center">
+		<h1 class="text-center text-black" id="hero">EXCLUSIVLEY<br /> MINTING ONLY  <br /> ON BASE </h1>
+	  </div>
+	</div>
+  </div>
+  
+  <!-- Stacked layout for smaller screens -->
+  <div class="md:hidden">
+	<div class="grid grid-cols-1 mx-auto mt-12">
+	  <div class="flex justify-center items-center">
+		<h1 class="text-center text-black" id="hero">GIVING CRYPTO <br /> A FACE THROUGH <br /> THROUGH TOKENEYES</h1>
+	  </div>
+  
+	  <div class="flex justify-center mt-24 px-8">
+		<img src="../images/eth.png" alt="xrp" class="max-w-auto" />
+	  </div>
+  
+	  <div class="flex justify-center items-center mt-12 mb-12">
+		<h1 class="text-center text-black" id="hero">EXCLUSIVLEY <br /> MINTING ONLY <br /> ON BASE</h1>
+	  </div>
+	</div>
+  </div>
+  
